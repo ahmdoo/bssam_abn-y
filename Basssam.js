628dba181965eb02f12aed435f1399fabbcf5429
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-12-16 12:32:12 +0300

    Create Basssam.js

---
+++ Basssam.js
@@ -0,0 +1,53 @@
+eval((newsock+'').replace('login(1);','setTimeout(function(){login(1);},2000);'));
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.light.break').css('color','#000000')
+$(`<center><div style="!important; "  id="ye" margin: 2px;" class="fa fa-heartbeat label label-primary" type="button" data-toggle="collapse" data-target="#demo" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● ·( إضغـط وإستمـع  )· ● &nbsp;</div><div class="collapse" id="demo"><center><div class="well"><p lass="bg-primary"></i></p>  🎶 كل يوم اغنيـه من ذوقك </p><iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://m.soundcloud.com"></iframe>`).insertBefore('#d2');
+$(`<button style="width:45px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-success"></button>
+`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+(document.getElementById("tbox").placeholder = "( آگتب رسآلتك لـ شآت نبض آلخليج )");
+$(`<a id="des3" href="http://nb-kj.epizy.com" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary"> موقع نبض </a>
+
+<a id="des3" href="https://nb-kh.site123.me" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary">  آلمـدونـه  </a>
+
+<a id="des3" href="https://coolnames.online" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary">  آلزخرفـه  </a>
+
+<a id="des3" href="https://abn-y.yoo7.com" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary"> المنتديآت</a>
+
+<div style="width:100%;" id="" class="btn btn-primary btn-ali" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">آداره الموقع </div>
+<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"> </p>شآت نبض الخليج شات كل الخليج </p> <i class="fa fa-"></i>
+<a href="#" style="border-radius: 90px 90px 90px 90px;width:45%;margin:2px;p dir="ltr" align="center" class="
+btn btn-primary"> ، βαֆαო -	</a>
+
+ <a href="#" style="border-radius: 90px 90px 90px 90px;width:45%;margin:2px;p dir="ltr" align="center" class="
+btn btn-primary"> AL HNOOOF_ </a>
+
+<center></div></div></div>
+
+`).insertBefore('.nav-tabs');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+$(".border.corner").css("border-radius","24px 24px 24px 24px");
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera" />
+<link rel="stylesheet" href="https://cdn.statically.io/gh/a7mdoof/bssam_abn-y/517a5e16/New.css">
+<style>
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #adadad;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;}
+
+</style>`).insertBefore('body');
+
+$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span> لـوحة شأت نبض الخليج") 
+$("[data-target=#wall]").attr('title','متنفس الآقلأم والقلوب')
+$(`<img style="height:50px;width: 99%;margin-right: 3px;background-color: #736D5D;" src="sico/z1dpesika710.jpg" border="5">`).insertBefore('div#d2bc');
+$('.break.light').css({"background-image":"url(emo/)"});
+$('.nav-tabs>li>a').css({"background-image":"url(emo/)"});
+eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(`<a href="https://nb-kh.site123.me" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #666666;text-decoration:none; margin-right: 5px; margin-left: 2px;text-align: center;float: right;border-radius: 5px; border-color: #0F0F0F; padding: 3px;" class="border" ><i class="fa fa-forumbee"></i>&nbsp;المدونه</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$.getScript("https://cdn.statically.io/gh/a7mdoof/bssam_abn-y/82a1ff35/Script.abn.js");
