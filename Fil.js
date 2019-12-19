9bac22ccf3e6ae7a6dbcecd413ebbf5f29e98cfc
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-12-19 21:45:38 +0300

    Create Fil.js

---
+++ Fil.js
@@ -0,0 +1,73 @@
+var Wally = function(){
+  $(".btn.minix.btn-primary.fa").css("margin-top",-21);
+$(".btn.minix.btn-primary.fa").css("margin-left",230);
+$(".btn.minix.btn-primary.fa.fa-comment.fr").css("margin-top",15);
+
+$(".btn.minix.btn-primary.fa.fa-comment.fr").css("margin-right",-25);
+};
+setInterval(Wally, 7000);
+
+$(`<span class="rating-full-star"></span>`).insertBefore('.ulike.borderg');
+
+
+
+$('.border.fl.likebox').css({"background-image":"url(sico/z1dsdad8qb10.jpeg)"});
+
+
+$('.border.fl.powerbox').css({"background-image":"url(sico/z1dsdad8qb10.jpeg)"});
+
+
+
+$('.border.fl.roomzbox').css({"background-image":"url(sico/z1dsdad8qb10.jpeg)"});
+$(".uroomz.border").text('  نقــل للغرفه ');
+$(".upower.border").text('حفظ الصلاحيات');
+$(".ulikec.border").text('حفظ اللايكات');
+$(`<style>
+.rpwd {width: 60%!important;border-radius: 50px!important;border:2px solid;}
+.uroomz.border {width: fit-content!important;border-radius: 50px!important;margin-top: -46px!important;margin-left: 200px!important;}
+.roomz.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.upower.border {width: fit-content!important;border-radius: 50px!important;margin-top: -47px!important;margin-left: 190px!important;}
+.userpower.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.userdays {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ulikec.border {width: fit-content!important;border-radius: 50px!important;border:2px solid;}
+.likec {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ae.fa.chats.label.fa-comment.label-primary {border-radius: 0px 30px 30px 0px!important}
+.ae.fa.label.label-primary.fa-user {border-radius: 30px 0px 0px 30px!important;}
+.ae.fa.label.label-primary.fa-users {border-radius: 30px 0px 0px 30px!important;margin-left: 40px!important;}
+.ae.fa.label.label-primary.fa-commenting-o {border-radius: 0px 0px 0px 0px!important;margin-left: 0px!important;}
+.ae.label.label-primary.fa.fa-gear {border-radius: 0px 30px 30px 0px!important;margin-left: 0px!important;}
+.nav.nav-tabs {margin-top: 2px!important;}
+.btn.btn-primary.btn-ali {border: 2px solid #ffffff!important;height: 5%!important;width: 70%!important;margin-top: 0px!important;background-color: #ffffff!important;color: #000000!important;background-image:-webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent) !important;}
+
+.a.label.label-primary.fl
+	{
+	display:none
+}
+button.btn.fr.btn-success.fa.fa-refresh
+	{
+	display:none
+}
+a.label.label-primary.fl img.fl
+	{
+	display:none
+}
+a.label.label-primary.fl
+	{
+	border-radius:3px!important;
+	margin:2px-0px;
+	padding:5px!important;
+	margin-bottom:2px;
+	text-align:right!important
+}
+</style>`).insertBefore('body');
+// شفرتهم!! بس خليت لك كود البنر اللي برا والحائط غير بكيفك //
+$(`<a id="ahmed" href="" style="color:#000000;"border-radius: 24px;width:100%;margin-right: 100px!important;" ><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i>
+<span class="sr-only">Refreshing...</span>‎ ‎ 
+  تحـديـث‎‎</a><img src="sico/z1dpeq9gil10.gif" class="fr"
+ 
+style="Height:22%;width:71%;border-radius: 15px 0px 0px 15px!important;border:3px solid;background-color: # 946841;margin-top: 12px;">`).insertBefore('.nav-tabs');
+$(` <center><div width="90%" id="asim7"><font style="background-color: #FFFFFF;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 
+10px 7px 10px;" > مبدعة الحآئط  ( آمـولـه  ، )  </font></div><center>`).insertBefore('div#d2bc');
+//اللي تحت لاتقررربه ابد //
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$.getScript("https://cdn.statically.io/gh/a7mdoof/bssam_abn-y/c8b197e0/Basssam.js");
