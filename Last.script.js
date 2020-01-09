74f9875ac947b4d5c5dff68a18a8d7d932add2f1
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2020-01-09 17:58:06 +0300

    Create Last.script.js

---
+++ Last.script.js
@@ -0,0 +1,109 @@
+eval((newsock+'').replace('login(1);','setTimeout(function(){login(1);},2000);'));
+var Wally = function(){
+  $(".btn.minix.btn-primary.fa").css("margin-top",-21);
+$(".btn.minix.btn-primary.fa").css("margin-left",230);
+$(".btn.minix.btn-primary.fa.fa-comment.fr").css("margin-top",15);
+
+$(".btn.minix.btn-primary.fa.fa-comment.fr").css("margin-right",-25);
+};
+setInterval(Wally, 7000);
+
+$(`<span class="rating-full-star"></span>`).insertBefore('.ulike.borderg');
+
+
+
+$('.border.fl.likebox').css({"background-image":"url(sico/z1dsdad8qb10.jpeg)"});
+
+
+$('.border.fl.powerbox').css({"background-image":"url(sico/z1dsdad8qb10.jpeg)"});
+
+
+
+$('.border.fl.roomzbox').css({"background-image":"url(sico/z1dsdad8qb10.jpeg)"});
+$(".uroomz.border").text('  نقــل للغرفه ');
+$(".upower.border").text('حفظ الصلاحيات');
+$(".ulikec.border").text('حفظ اللايكات');
+$(`<style>
+.rpwd {width: 60%!important;border-radius: 50px!important;border:2px solid;}
+.uroomz.border {width: fit-content!important;border-radius: 50px!important;margin-top: -46px!important;margin-left: 200px!important;}
+.roomz.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.upower.border {width: fit-content!important;border-radius: 50px!important;margin-top: -47px!important;margin-left: 190px!important;}
+.userpower.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.userdays {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ulikec.border {width: fit-content!important;border-radius: 50px!important;border:2px solid;}
+.likec {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ae.fa.chats.label.fa-comment.label-primary {border-radius: 0px 30px 30px 0px!important}
+.ae.fa.label.label-primary.fa-user {border-radius: 30px 0px 0px 30px!important;}
+.ae.fa.label.label-primary.fa-users {border-radius: 30px 0px 0px 30px!important;margin-left: 40px!important;}
+.ae.fa.label.label-primary.fa-commenting-o {border-radius: 0px 0px 0px 0px!important;margin-left: 0px!important;}
+.ae.label.label-primary.fa.fa-gear {border-radius: 0px 30px 30px 0px!important;margin-left: 0px!important;}
+.nav.nav-tabs {margin-top: 2px!important;}
+.btn.btn-primary.btn-ali {border: 2px solid #ffffff!important;height: 5%!important;width: 70%!important;margin-top: 0px!important;background-color: #ffffff!important;color: #000000!important;background-image:-webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent) !important;}
+
+.a.label.label-primary.fl
+	{
+	display:none
+}
+button.btn.fr.btn-success.fa.fa-refresh
+	{
+	display:none
+}
+a.label.label-primary.fl img.fl
+	{
+	display:none
+}
+a.label.label-primary.fl
+	{
+	border-radius:3px!important;
+	margin:2px-0px;
+	padding:5px!important;
+	margin-bottom:2px;
+	text-align:right!important
+}
+</style>`).insertBefore('body');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.light.break').css('color','#000000')
+$(`<center><div style="!important; "  id="ye" margin: 2px;" class="fa fa-heartbeat label label-primary" type="button" data-toggle="collapse" data-target="#demo" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● ·( إضغـط وإستمـع  )· ● &nbsp;</div><div class="collapse" id="demo"><center><div class="well"><p lass="bg-primary"></i></p>  🎶 كل يوم اغنيـه من ذوقك </p><iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://m.soundcloud.com"></iframe>`).insertBefore('#d2');
+$(`<button style="width:45px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-success"></button>
+`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+(document.getElementById("tbox").placeholder = "( آگتب رسآلتك لـ شآت نبض آلخليج )");
+$(`<a id="des3" href="http://nb-kj.epizy.com" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary"> موقع نبض </a>
+
+<a id="des3" href="https://nb-kh.site123.me" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary">  آلمـدونـه  </a>
+
+<a id="des3" href="https://coolnames.online" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary">  آلزخرفـه  </a>
+
+<a id="des3" href="https://abn-y.yoo7.com" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary"> المنتديآت</a>
+
+<a id="ahmed" href="https://cdn.statically.io/gh/a7mdoof/bssam_abn-y/330675d2/Bg.index.html" target="_blank"style="border-radius: 24px;width: fit-content;margin:2px;" class="fl mini  fa fa-heartbeat
+btn btn-primary"> ادارة الموقع</a>
+
+
+<center></div></div></div>
+
+`).insertBefore('.nav-tabs');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+$(".border.corner").css("border-radius","24px 24px 24px 24px");
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera" />
+<link rel="stylesheet" href="https://cdn.statically.io/gh/a7mdoof/bssam_abn-y/ff0ec7bf/For.bsam.css">
+<style>
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #adadad;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;}
+
+</style>`).insertBefore('body');
+
+$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span> لـوحة شأت نبض الخليج") 
+$("[data-target=#wall]").attr('title','متنفس الآقلأم والقلوب')
+$(`<img style="height:50px;width: 99%;margin-right: 3px;background-color: #736D5D;" src="sico/z1dpesika710.jpg" border="5">`).insertBefore('div#d2bc');
+$('.break.light').css({"background-image":"url(emo/)"});
+$('.nav-tabs>li>a').css({"background-image":"url(emo/)"});
+$(`<a href="https://nb-kh.site123.me" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #666666;text-decoration:none; margin-right: 5px; margin-left: 2px;text-align: center;float: right;border-radius: 5px; border-color: #0F0F0F; padding: 3px;" class="border" ><i class="fa fa-forumbee"></i>&nbsp;المدونه</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$.getScript("https://cdn.statically.io/gh/a7mdoof/bssam_abn-y/82a1ff35/Script.abn.js");
